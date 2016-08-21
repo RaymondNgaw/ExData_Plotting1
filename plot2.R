e6208e46163e0c6d6114a631f6a0e71eeb00218c
rngaw@hotmail.com <rngaw@hotmail.com>
2016-08-21 01:16:52 -0700

    Plot 2

--- plot2.R
+++ plot2.R
@@ -1,13 +1,25 @@
-setwd('~/GitHub/project')
+# Read data, convert dates, fix for missing observations and subset for 2 days in February 2007
+data <- read.table('~/project/household_power_consumption.txt', sep=';', header=T, 
+                   colClasses = c('character', 'character', 'numeric',
+                                  'numeric', 'numeric', 'numeric',
+                                  'numeric', 'numeric', 'numeric'),
+                   na.strings='?')
+data$DateTime <- strptime(paste(data$Date, data$Time), 
+                          "%d/%m/%Y %H:%M:%S")
+data <- subset(data, 
+               as.Date(DateTime) >= as.Date("2007-02-01") & 
+                 as.Date(DateTime) <= as.Date("2007-02-02"))
 
-if (!file.exists('plots')) {
-  dir.create('plots')
-}
+# Open plot2.png
+png("plot2.png", height=480, width=480)
 
-source('~/project/cleaned.R')
+# Create time series
+plot(data$DateTime, 
+     data$Global_active_power, 
+     pch=NA, 
+     xlab="", 
+     ylab="Global Active Power (kilowatts)")
+lines(data$DateTime, data$Global_active_power)
 
-png(filename='plots/plot2.png',width=480,height=480,units='px')
-
-plot(power.consumption$DateTime,power.consumption$GlobalActivePower,ylab='Global Active Power (kilowatts)', xlab='', type='l')
-
-x<-dev.off()
+# Close PNG file
+dev.off()
